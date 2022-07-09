9e577d0fa2166d1777072041c7265c500dd9cb1b
legyouu <80972902+legyouu@users.noreply.github.com>
2022-07-09 19:58:19 +0200

    Add files via upload

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -33,7 +33,7 @@ class YukkiBot(Client):
         self.id = get_me.id
         try:
             await self.send_message(
-                config.LOG_GROUP_ID, "◍ تم تشغيل البوت علي سورس فمبير √"
+                config.LOG_GROUP_ID, "◍ تم تشغيل البوت علي سورس اوليانو √"
             )
         except:
             LOGGER(__name__).error(
